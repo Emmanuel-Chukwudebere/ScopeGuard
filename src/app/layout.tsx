01bc29afcf2212f24f21e374055215eeda2e117b
Emmanuel Chukwudebere <immanuelonyinyechi@gmail.com>
2026-03-19 20:46:03 +0100

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: "ScopeGuard",
+  description: "Eliminate scope creep. Get paid.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("dark", "font-sans", geist.variable)}>
+      <body>{children}</body>
+    </html>
+  );
+}
